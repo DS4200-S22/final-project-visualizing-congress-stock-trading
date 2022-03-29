9b3220648f2b6f1b8f4dbe15e27d69e8597b2876
Nikola Dobrev <dnikola43@gmail.com>
2022-03-29 13:35:26 -0400

    add gary's files

--- js/visualization.js
+++ js/visualization.js
@@ -1,6 +1,5 @@
-//This is filler -- delete it and start coding your visualization tool here
-d3.select("#vis-container")
-  .append("text")
-  .attr("x", 20)
-  .attr("y", 20)
-  .text("Hello World!");
+d3.csv("data/Congressional Trading.csv").then((data) => {
+  for (let i = 0; i < 10; i++) {
+  console.log(data[i])
+}
+})
